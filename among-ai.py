a4ea1223dbba934ad453d9791eb8b87d8d03455f
nickrister22 <55163230+nickrister22@users.noreply.github.com>
2020-11-10 23:08:50 -0600

    starting search

--- among-ai.py
+++ among-ai.py
@@ -7,6 +7,13 @@ from test import load_graph
 input_data = []
 num_of_players = 3
 
+
+
+
+with open(f"./graph-1/au-test-{num_of_players}-1000.json", "r") as file:
+    input_data = json.load(file)
+
+
 graph = load_graph("./graph-1/test_nodes.txt", "./graph-1/test_edges.txt")
     
 for game in input_data:
@@ -38,3 +45,36 @@ class PlayerTimelines():
             def __init__(self, player):
                 self.player = player
                 self.Timeline = []
+
+class Search():
+    def __init__(self, graph, start, goal):
+        self.graph = graph
+        self.start = start
+        self.goal = goal
+
+    def searchGoal(self):
+
+        currentNode = graph.nodes[self.start]
+        goalNode = graph.nodes[self.goal]
+
+        visited = []
+        frontier = []
+        path = {}
+
+        frontier.append(currentNode)
+
+        while len(frontier) > 0:
+
+            currentNode = frontier.pop(0)
+
+            if currentNode in visited:
+                continue
+
+            for node, weight in graph.nodes[currentNode.get_name()]:
+                if goalNode == node:
+                    return path
+                frontier.append(node)
+
+            visited.append(currentNode)
+
+
