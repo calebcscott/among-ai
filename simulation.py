60853f72f438fc7168fe8c279cea7edeb3a0d4b9
Christopher Scott <chris.c.scott@outlook.com>
2020-11-10 18:08:54 -0600

    Init

---
+++ simulation.py
@@ -0,0 +1,74 @@
+import re
+
+
+# Defnie Base Node Class
+""" Node object to contain information of the current Node i.e. Name, Position(x,y), attributes, etc. 
+        Edges not an attribute of Node but Attribute of Graph
+"""
+class BaseNode():
+    def __init__(self,
+        name,
+        *args
+    ):
+        self.name = name
+        self.args = args
+
+    def get_name(self):
+        return self.name
+
+
+# Define Graph Class
+
+class Graph():
+    def __init__(self, NodeType=BaseNode):
+        self.nodes = {}
+        self.edges = {}
+        self.NodeType = NodeType
+        pass
+        # Nodes: Name of Node -> Node Object
+        # Edges: Name of Node -> List of Connected Nodes in form (Node, weight)
+        # iter returns (Current Node, List of Edges)   
+
+    def add_node(self, name, node):
+        self.nodes[name] = node
+    
+    def add_edge(self, name_base_node, edge_node, weight=1):
+        if self.edges.get(name_base_node, None):
+            self.edges[name_base_node].append((edge_node, weight))
+        else:
+            self.edges[name_base_node] = [(edge_node, weight)]
+
+
+    def load_nodes(self, filename, re_format):
+        pat = re.compile(re_format)
+        with open(filename, "r") as node_file:
+            for line in node_file:
+                match = pat.match(line.rstrip())
+                if match:
+                    self.add_node(match.group(1), self.NodeType(*match.groups()))
+                
+
+
+    def load_edges(self, filename, re_format):
+        pat = re.compile(re_format)
+        with open(filename, "r") as edge_file:
+            for line in edge_file:
+                match = pat.match(line.rstrip())
+                if match: 
+                    self.add_edge(match.group(1), self.nodes[match.group(2)], int(match.group(3)) if len(match.groups()) > 2 else 1)
+                    self.add_edge(match.group(2), self.nodes[match.group(1)], int(match.group(3)) if len(match.groups()) > 2 else 1)
+
+
+    def write_graph(self, filename):
+        """ possibly write out graph structure to file (i.e. Nodes and Edges) """
+        pass
+
+
+    def __iter__(self):
+        for _, node in self.nodes.items():
+            yield (node, self.edges.get(node.get_name(), []))
+
+# Graph Class will read in Nodes from serialized file
+# Allows for dynamic Graph generation
+# Graph Class will read in Edges from file
+# Allows for dynamic changes to Graph without changing Code
---
+++ test.py
@@ -0,0 +1,120 @@
+from simulation import Graph, BaseNode
+import random
+import pprint
+import sys
+import json
+
+class SecurityCamera(BaseNode):
+    def __init__(self, name, camera, *args):
+        self.camera = camera.lower() == 'true'
+        super().__init__(name, args)
+
+    def is_camera(self):
+        return self.camera
+
+def load_graph(node_file, edge_file):
+
+    g = Graph(SecurityCamera)
+
+    g.load_nodes(node_file, r'([\w ]+),([\w ]+)')
+    g.load_edges(edge_file, r'([\w ]+),([\w ]+)')
+
+    return g
+
+def create_players(number, starting_node):
+    players = {}
+    print(f'Creating {number} of players for current game')
+    for i in range(1, number+1):
+        players[i] = starting_node
+    return players
+
+
+def update_times(attributes, node_name, player, time):
+    print(f'Updating time for {player} in node {node_name}')
+    attributes[node_name].append( (player, time) )
+
+def game(g, num_of_players):
+    starting_node = random.choice(list(g.nodes))
+    attributes = {
+        "Dead" : "",
+        "Found" : False,
+        "tFound" : 0,
+        "Location" : ""
+    }
+
+    camera_count = 0
+    for node, _ in g :
+        if node.is_camera():
+            camera_count += 1
+            attributes[node.get_name()] = []
+
+    players = create_players(num_of_players, starting_node)
+
+    # if starting node camera set all times in beginning
+    if starting_node in attributes:
+        for player, starting_node in players.items():
+            update_times(attributes, starting_node, player, 1)
+
+    killer = random.choice(list(players))
+
+    buffer = 30
+    time = 1
+
+    while not attributes["Found"]:
+        # move all players and log times
+        for player, node in players.items():
+            # move players
+            if player is attributes["Dead"]:
+                print(f'Player: {player} is dead not moving')
+                continue
+            choice =  random.choice(g.edges[node])[0]
+            players[player] = choice.get_name()
+
+            # log movement
+            if g.nodes[choice.get_name()].is_camera():
+                update_times(attributes, choice.get_name(), player, time)
+
+            # check for body
+            if attributes["Dead"] and choice.get_name() == players[attributes["Dead"]] and not attributes["Found"]:
+                attributes["Found"] = True
+                attributes["tFound"] = time 
+                attributes["Location"] = choice.get_name()
+        
+        # determine if killer can kill
+        if time > buffer and not attributes["Dead"]:
+            killer_node = players[killer]
+            victims = []
+
+            for player, node in players.items():
+                if player != killer and node == killer_node:
+                    victims.append(player)
+            print(f'Possible victims for killer {victims}')
+            if 0 < len(victims) < 2:
+                print(f'Killed Player {victims[0]} at time {time}')
+                attributes["Dead"] = victims[0]
+        
+        # increment time
+        time += 1
+
+    return (attributes, killer)
+
+
+if __name__ == "__main__":
+    # test.py node_file.txt edge_file.txt out_file.txt {# of players} {# of games}
+    node_file = sys.argv[1]
+    edge_file = sys.argv[2]
+    out_file = sys.argv[3]
+    num_of_players = int(sys.argv[4])
+    num_of_games = int(sys.argv[5])
+
+    game_output = []
+    print(node_file)
+    graph = load_graph(node_file, edge_file)
+    for i in range(num_of_games):
+        game_out = game(graph, num_of_players)
+        game_output.append(game_out)
+
+    pprint.pprint(game_output)
+
+    with open(f'{out_file}-{num_of_players}-{num_of_games}.json', "w") as file:
+        json.dump(game_output, file)
