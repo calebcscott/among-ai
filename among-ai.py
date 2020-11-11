8e94d918c439294efb9d1dc755536c1e1198c38a
owinterbg <36868567+owinterbg@users.noreply.github.com>
2020-11-10 23:17:32 -0600

    fixing things

--- among-ai.py
+++ among-ai.py
@@ -1,40 +1,65 @@
 import json
 import pprint
 from typing import List
-from simulation import Graph
+from simulation import Graph, Game
 from test import load_graph
 
-input_data = []
-num_of_players = 3
-
-graph = load_graph("./graph-1/test_nodes.txt", "./graph-1/test_edges.txt")
-    
-for game in input_data:
-    pprint.pprint(game)
-    break
-
-def loadInputData(fileName):
-    file = open(fileName)
-
 class PlayerTimelines():
-    def __init__(self, game):
-        self.timelines: list[self.Player] = []
+    def __init__(self, game: Game, killer):
+        self.timelines: List[self.Player] = []
         self.game = game
-
-        self.loadPlayerTimelines()
+        self.killer = killer
 
     def loadPlayerTimelines(self):
         self.timelines = []
-        for location, player, time in game[0].Events:
-            if player in map(lambda player: player.player, self.timelines):
-                index = self.timelines.index(player, map(lambda player: player.player, self.timelines))
+        for location, player, time in self.game.events:
+            if player in list(map((lambda player: player.player), self.timelines)):
+                index = list(map(lambda player: player.player, self.timelines)).index(player)
                 self.timelines[index].Timeline.append({
                     time: time,
                     location: location
                 })
+            else:
+                data = self.Player(player)
+                data.Timeline.append({
+                    time: time,
+                    location: location
+                })
+                self.timelines.append(data)
             
 
     class Player():
             def __init__(self, player):
                 self.player = player
                 self.Timeline = []
+            
+            def __str__(self) -> str:
+                returnString = 'Player:\t' + str(self.player) + '\n'
+                for time, location in self.Timeline:
+                    returnString += '\tTime: ' + str(time) + ' Location: ' + location + '\n'
+                return returnString
+
+    def __str__(self) -> str:
+        returnString = 'Killer: ' + str(self.killer) + ' Victim:' + str(self.game.dead_player) + '\n'
+        for player in self.timelines:
+            returnString += str(player)
+
+        return returnString
+
+def loadInputData(fileName) -> List[PlayerTimelines]:
+    inputData = []
+    with open(f'time_graph_test-10-1.json', 'r') as file:
+        data = json.load(file)
+    for game, killer in data:
+        inputData.append(PlayerTimelines(Game(game, ''), killer))
+    return inputData
+
+
+
+if __name__ == "__main__":
+    inputData: List[PlayerTimelines] = loadInputData('time_graph_test-10-1.json')
+    graph = load_graph("./graph-1/test_nodes.txt", "./graph-1/test_edges.txt")
+
+    for data in inputData:
+        data.loadPlayerTimelines()
+        print(data)
