8c4bc9169367a0a09aba896c2941b28418a1eb69
owinterbg <36868567+owinterbg@users.noreply.github.com>
2020-11-10 21:51:11 -0600

    PlayerTimelines class
    add class to represent a game and fragmented timelines of players observed in a game.

--- among-ai.py
+++ among-ai.py
@@ -1,5 +1,6 @@
 import json
 import pprint
+from typing import List
 from simulation import Graph
 from test import load_graph
 
@@ -18,3 +19,26 @@ graph = load_graph("./graph-1/test_nodes.txt", "./graph-1/test_edges.txt")
 for game in input_data:
     pprint.pprint(game)
     break
+
+class PlayerTimelines():
+    def __init__(self, game):
+        self.timelines: list[self.Player] = []
+        self.game = game
+
+        self.loadPlayerTimelines()
+
+    def loadPlayerTimelines(self):
+        self.timelines = []
+        for location, player, time in game[0].Events:
+            if player in map(lambda player: player.player, self.timelines):
+                index = self.timelines.index(player, map(lambda player: player.player, self.timelines))
+                self.timelines[index].Timeline.append({
+                    time: time,
+                    location: location
+                })
+            
+
+    class Player():
+            def __init__(self, player):
+                self.player = player
+                self.Timeline = []
